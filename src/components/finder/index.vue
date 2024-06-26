d7a18a2d6165121e058339b8d2421438f15f779c
ouou12138 <65159211+ouou12138@users.noreply.github.com>
2024-06-26 19:43:50 +0800

    feat: 🎸 继续开发 finder app

--- src/components/finder/index.vue
+++ src/components/finder/index.vue
@@ -2,62 +2,85 @@
   <div class="w-full h-full flex">
     <div class="side-bar w-208px h-full flex flex-col">
       <div class="placeholder h-55px"></div>
-      <div class="px-16px overflow-auto h-fit flex-1">
-        <div class="not-first-of-type-pt-3 category">
-          <div class="text-2.5 text-#666">个人收藏</div>
+      <div class="px-16px overflow-auto h-fit flex-1" @mousedown.stop="() => null">
+        <div
+          v-for="(category, index) in sideBarData"
+          :key="index"
+          class="not-first-of-type-pt-3 category"
+        >
+          <div class="text-2.5 text-#666">{{ category.category }}</div>
           <ul class="items list-none m-0 text-3.25">
-            <li class="active">
+            <li
+              v-for="(item, index) in category.items"
+              :key="index"
+              :class="{ active: activeName === item.name }"
+              @click="() => (activeName = item.name)"
+            >
               <p class="icon"></p>
-              最近使用
-            </li>
-            <li>
-              <p class="icon"></p>
-              应用程序
-            </li>
-            <li>
-              <p class="icon"></p>
-              桌面
-            </li>
-            <li>
-              <p class="icon"></p>
-              下载
-            </li>
-            <li>
-              <p class="icon"></p>
-              下载
-            </li>
-          </ul>
-        </div>
-        <div class="not-first-of-type-pt-3 category">
-          <div class="text-2.5 text-#666">iCloud</div>
-          <ul class="items list-none m-0 text-3.25">
-            <li>
-              <p class="icon"></p>
-              iCloud云盘
-            </li>
-            <li>
-              <p class="icon"></p>
-              共享
-            </li>
-          </ul>
-        </div>
-        <div class="not-first-of-type-pt-3 category">
-          <div class="text-2.5 text-#666">位置</div>
-          <ul class="items list-none m-0 text-3.25">
-            <li>
-              <p class="icon"></p>
-              Data
+              {{ item.name }}
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="content bg-white flex-1 h-full" @mousemove.stop></div>
+    <div class="content flex-1 h-full flex flex-col">
+      <div class="top-bar flex items-center box-border px-20px h-55px bg-#F6F8F7">
+        <div class="text-3.8 text-200">{{ activeName }}</div>
+      </div>
+      <div class="flex-1 bg-white" @mousedown.stop="() => null"></div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
+
+const sideBarData = reactive([
+  {
+    category: '个人收藏',
+    items: [
+      {
+        name: '最近使用',
+        icon: ''
+      },
+      {
+        name: '应用程序',
+        icon: ''
+      },
+      {
+        name: '桌面',
+        icon: ''
+      },
+      {
+        name: '下载',
+        icon: ''
+      }
+    ]
+  },
+  {
+    category: 'iCloud',
+    items: [
+      {
+        name: 'iCloud云盘',
+        icon: ''
+      },
+      {
+        name: '共享',
+        icon: ''
+      }
+    ]
+  },
+  {
+    category: '位置',
+    items: [
+      {
+        name: 'Data',
+        icon: ''
+      }
+    ]
+  }
+])
+const activeName = ref('最近使用')
 </script>
 
 <style lang="scss" scoped>
@@ -69,10 +92,11 @@ import { reactive, ref } from 'vue'
     padding-left: 0;
     color: #333;
     li {
-      padding: 5px;
+      padding: 3px 5px;
       display: flex;
       align-items: center;
       height: 20px;
+      margin: 2px 0;
       .icon {
         width: 12px;
         height: 12px;
@@ -81,7 +105,7 @@ import { reactive, ref } from 'vue'
         margin-right: 5px;
       }
       &.active {
-        background-color: #c8cbd0;
+        background-color: rgba($color: #c8cbd0, $alpha: 0.6);
         border-radius: 5px;
       }
     }
